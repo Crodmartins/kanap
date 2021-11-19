3e1b2f232bb11dc942e2e9613c4310f28ffe7fd2
Cecile Martins <rmartinscecile@gmail.com>
2021-11-19 18:46:54 +0100

    affichage produit sauf couleur

--- front/js/product.js
+++ front/js/product.js
@@ -7,7 +7,9 @@ let UrlProduct = `http://localhost:3000/api/products/${id}`;
 console.log(UrlProduct);
 
 let photo = document.querySelector('.item__img')    
-const baliseImg = document.createElement('img');
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let description = document.getElementById('description');
 
 /*recevoir les données*/
 fetch (UrlProduct)                                            
@@ -18,7 +20,9 @@ fetch (UrlProduct)
 })
   .then(function(product) {
     photo.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
-    
+    title.innerHTML = `<h1 id="title">${product.name}</h1>`
+    price.innerHTML = `<span id="price">${product.price}</span>€</p>`
+    description.innerHTML = `<p id="description">${product.description}</p>`
   })
   .catch(function(err) {
     // Une erreur est survenue
