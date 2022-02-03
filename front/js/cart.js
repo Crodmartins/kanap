0f79238cdde853c3c7efef14db89ff372fb2ddc3
Cecile Martins <rmartinscecile@gmail.com>
2022-02-03 11:58:25 +0100

    changement fetch en async await

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,5 @@
 /*AFFICHAGE PANIER*/
 let productsInCart = JSON.parse(localStorage.getItem('cart'));//je récupère les données du local storage
-console.log(productsInCart);
 
 const cartItems = document.getElementById('cart__items');//je sélectionne le code html à modifier
 
@@ -8,74 +7,82 @@ for (let i = 0; i < productsInCart.length; i++) {//je créé une boucle pour cha
   let id = productsInCart[i].id;
   let UrlProduct = `http://localhost:3000/api/products/${id}`; //j'appelle l'API en fonction de l'ID du produit au panier
   
-fetch (UrlProduct) //j'appelle l'API pour récupérer les données                                          
-   .then(function(res) {
-    if (res.ok) {
+async function myFetch() {
+  let res = await fetch(UrlProduct);
+
+  if (!res.ok) {
     return res.json();
-    }
-  })
-    .then(function(product) {
-      cartItems.innerHTML += `
+  }
+  let product = await res.json();
+    cartItems.innerHTML += `
       <article class="cart__item" data-id="${productsInCart[i].id}">
-                <div class="cart__item__img">
-                  <img src="${product.imageUrl}" alt="${product.alt}">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__titlePrice">
-                    <h2>${product.name}</h2>
-                    <h3>${productsInCart[i].color}<h3>
-                    <p>${product.price} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productsInCart[i].quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article>
-              `;
-            })
-    .catch(function(err) {
-      // Une erreur est survenue
-    });
+        <div class="cart__item__img">
+          <img src="${product.imageUrl}" alt="${product.alt}">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__titlePrice">
+            <h2>${product.name}</h2>
+            <h3>${productsInCart[i].color}<h3>
+            <p>${product.price} €</p>
+        </div>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productsInCart[i].quantity}">
+           </div>
+          <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
+          </div>
+        </div>
+        </div>
+      </article>
+      `;
+    
+}
+ 
+myFetch()
+.catch (e => {
+  console.log('une erreur est survenue');
+});
 }
 
-/*CALCUL DE LA QUANTITE*/
+//CALCUL DE LA QUANTITE
 
 let totalItems = 0;
-  
+
 for (let i = 0; i < productsInCart.length; i++) {//je calcule les quantités totales
-  totalItems += productsInCart[i].quantity;
-  }
+totalItems += productsInCart[i].quantity;
+}
 
-  let totalQuantity = document.getElementById('totalQuantity');
-  totalQuantity.innerHTML = totalItems;//j'affiche la quantité totale
+let totalQuantity = document.getElementById('totalQuantity');
+totalQuantity.innerHTML = totalItems;//j'affiche la quantité totale
 
-/*CALCUL DU PRIX TOTAL*/
+
+//CALCUL DU PRIX TOTAL
 let totalAmount = 0;
+let totalPrice = document.getElementById('totalPrice');
 
 for (let i = 0; i < productsInCart.length; i++) {//je créé une boucle pour chaque produit
-  let id = productsInCart[i].id;
-  let UrlProduct = `http://localhost:3000/api/products/${id}`; //j'appelle l'API en fonction de l'ID du produit au panier
-  
-  let totalPrice = document.getElementById('totalPrice');
-  
-fetch (UrlProduct) //j'appelle l'API pour récupérer les données                                          
-   .then(function(res) {
-    if (res.ok) {
-    return res.json();
-    }
-  })
-    .then(function(product) {
-      totalAmount += (product.price * productsInCart[i].quantity);
-      totalPrice.innerHTML = totalAmount;
-    })
-    .catch(function(err) {
-      // Une erreur est survenue
-    });
+let id = productsInCart[i].id;
+let UrlProduct = `http://localhost:3000/api/products/${id}`; //j'appelle l'API en fonction de l'ID du produit au panier
 
+
+async function myFetch() {
+  let res = await fetch(UrlProduct);
+
+  if (!res.ok) {
+    return res.json();
   }
+  alert('res prix ok');
+  
+  let product = await res.json();
+    totalAmount += product.price * productsInCart[i].quantity;
+    totalPrice.innerHTML = totalAmount;
+  
+}
+
+myFetch()
+  .catch (e => {
+  console.log('une erreur est survenue');
+  });
+}
